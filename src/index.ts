2bbaeff5e98607f9722353d6397c69fcc114b84d
Joe Roddy <mrjoeroddy@gmail.com>
2023-06-02 11:07:03 -0400

    fix export

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,4 @@ import 'isomorphic-fetch';
 const run = (valName: string, ...args: any) =>
   fetch(`https://api.val.town/v1/run/${valName.replace('@', '')}?args=${JSON.stringify(args)}`).then((r) => r.json());
 
-export default {
-  run,
-};
-
-(async () => {
-  const res = await run('@joey.helloWorld', { a: 'some arg' });
-  console.log(res);
-})();
+export { run };
